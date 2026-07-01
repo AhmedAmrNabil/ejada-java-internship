d6ac500301ca43dd745bb37f99d9521a6a18bb38
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-07-01 15:14:38 +0300

    feat: migrate to devenv from flake
    needed to do this to creat mysql db on the fly for the project

---
+++ devenv.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }: {
+  languages.java = {
+    enable = true;
+    jdk.package = pkgs.jdk25;
+    maven.enable = true;
+  };
+
+  services.mysql = {
+    enable = true;
+    ensureUsers = [
+      {
+        name = "devenv";
+        ensurePermissions = {
+          "devenv.*" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+  };
+
+  enterShell = ''
+    #bash
+    mkdir -p .vscode
+    if [ ! -f .vscode/settings.json ]; then echo '{}' > .vscode/settings.json; fi
+
+    ${pkgs.jq}/bin/jq \
+      --arg jdkPath "${pkgs.jdk25}/lib/openjdk" \
+      '.["java.configuration.runtimes"] = [
+        { "name": "JavaSE-25", "path": $jdkPath }
+      ]
+      | .["java.jdt.ls.java.home"] = $jdkPath' \
+      .vscode/settings.json > .vscode/settings.json.tmp \
+      && mv .vscode/settings.json.tmp .vscode/settings.json
+  '';
+}
