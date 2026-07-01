2f186aa3ab1a37dc43ca91126f681e5d9aa85a8c
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-07-01 13:32:45 +0300

    feat: add shellHook to create VSCode settings for JavaSE-25

--- flake.nix
+++ flake.nix
@@ -27,6 +27,20 @@
               jdk25
               maven
             ];
+            shellHook = ''
+              #bash
+              mkdir -p .vscode
+              if [ ! -f .vscode/settings.json ]; then echo '{}' > .vscode/settings.json; fi
+
+              ${pkgs.jq}/bin/jq \
+                --arg jdkPath "${pkgs.jdk25}/lib/openjdk" \
+                '.["java.configuration.runtimes"] = [
+                  { "name": "JavaSE-25", "path": $jdkPath }
+                ]
+                | .["java.jdt.ls.java.home"] = $jdkPath' \
+                .vscode/settings.json > .vscode/settings.json.tmp \
+                && mv .vscode/settings.json.tmp .vscode/settings.json
+            '';
           };
         };
 
