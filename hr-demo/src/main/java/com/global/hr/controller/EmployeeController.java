6614c84de09806c906a1db1566540f32ee2b3d49
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-07-09 13:12:30 +0000

    feat(hr-demo): enhance EmployeeController with ResponseEntity for better HTTP response handling

--- hr-demo/src/main/java/com/global/hr/controller/EmployeeController.java
+++ hr-demo/src/main/java/com/global/hr/controller/EmployeeController.java
@@ -5,16 +5,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.global.hr.entity.Employee;
 import com.global.hr.repository.EmployeeRepository;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -30,28 +35,46 @@ public class EmployeeController {
 	}
 
 	@GetMapping
-	public Page<Employee> getEmployees() {
+	public ResponseEntity<Page<Employee>> getEmployees() {
 		log.info("/employees endpoint called");
-		return employeeRepository.findAll(Pageable.unpaged());
+		return ResponseEntity.ok(employeeRepository.findAll(Pageable.unpaged()));
 	}
 
 	@GetMapping("/{id}")
-	public Employee getEmployeeById(@PathVariable long id) {
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
 		log.info("/employees/{} endpoint called", id);
-		return employeeRepository.findById(id).get();
+		Optional<Employee> employee = employeeRepository.findById(id);
+		if (employee.isPresent()) {
+			return ResponseEntity.ok(employee.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 	@GetMapping("/search/{firstName}")
-	public List<Employee> getEmployeesByFirstName(@PathVariable String firstName) {
-		log.info("/employees/search/{} endpoint called", firstName);
-		return employeeRepository.findByFirstName(firstName);
+	public ResponseEntity<List<Employee>> getEmployeesByFirstName(@PathVariable String firstName) {
+		log.info("GET /employees/search/{} endpoint called", firstName);
+		return ResponseEntity.ok(employeeRepository.findByFirstName(firstName));
 	}
 
 	@PostMapping
-	public String postEmployee(@RequestBody Employee employee) {
-		log.info("/employees endpoint called with employee: {}", employee);
-		employeeRepository.save(employee);
-		return "Employee added successfully";
+	public ResponseEntity<Employee> postEmployee(@RequestBody Employee employee) {
+		log.info("POST /employees endpoint called with employee: {}", employee);
+		Employee savedEmployee = employeeRepository.save(employee);
+		URI location = URI.create("/employees/" + savedEmployee.getId());
+		return ResponseEntity.created(location).body(savedEmployee);
+	}
+
+	@PutMapping
+	public Employee putEmployee(@RequestBody Employee employee) {
+		log.info("PUT /employees endpoint called with employee: {}", employee);
+		return employeeRepository.save(employee);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable long id) {
+		log.info("DELETE /employees/{} endpoint called", id);
+		employeeRepository.deleteById(id);
 	}
 
 }
