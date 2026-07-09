ea1b9b04b4cbfba7da658935d1bd3aa338257bd3
AhmedAmrNabil <ahmedamr24680@gmail.com>
2026-07-09 07:50:38 +0000

    feat(hr-demo): add insert/update/delete for employee

--- hr-demo/src/main/java/com/global/hr/repository/EmployeeRepository.java
+++ hr-demo/src/main/java/com/global/hr/repository/EmployeeRepository.java
@@ -2,7 +2,6 @@ package com.global.hr.repository;
 
 import java.util.List;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -29,5 +28,38 @@ public class EmployeeRepository {
 		return jdbcTemplate.query(
 				"SELECT * FROM employees",
 				new EmployeeMapper());
+	}
+
+	public int count() {
+		return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM employees", Integer.class);
+	}
+
+	public void insert(Employee employee) {
+		jdbcTemplate.update(
+				"INSERT INTO employees (first_name, last_name, email, phone_number, hire_date, job_id, salary) VALUES (?, ?, ?, ?, ?, ?, ?)",
+				employee.getFirstName(),
+				employee.getLastName(),
+				employee.getEmail(),
+				employee.getPhoneNumber(),
+				employee.getHireDate(),
+				employee.getJobId(),
+				employee.getSalary());
+	}
+
+	public void update(Employee employee) {
+		jdbcTemplate.update(
+				"UPDATE employees SET first_name = ?, last_name = ?, email = ?, phone_number = ?, hire_date = ?, job_id = ?, salary = ? WHERE employee_id = ?",
+				employee.getFirstName(),
+				employee.getLastName(),
+				employee.getEmail(),
+				employee.getPhoneNumber(),
+				employee.getHireDate(),
+				employee.getJobId(),
+				employee.getSalary(),
+				employee.getId());
+	}
+
+	public void delete(long id) {
+		jdbcTemplate.update("DELETE FROM employees WHERE employee_id = ?", id);
 	}
 }
